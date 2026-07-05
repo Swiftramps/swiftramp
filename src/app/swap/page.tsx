7cb2ed02af69d01840606c26e8632d14987908ff
BernardOnuh <onuhbernard4@gmail.com>
2026-07-05 11:14:11 +0100

    cycle background color to #FFE8E8 / #FEE0E0

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -46,8 +46,8 @@ const CSS = `
   @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=IBM+Plex+Sans:wght@400;500;600&family=IBM+Plex+Mono:wght@500;600&display=swap');
 
   .swap-page {
-    --sw-bg-0: #F0FFE8;
-    --sw-bg-1: #E8FEE0;
+    --sw-bg-0: #FFE8E8;
+    --sw-bg-1: #FEE0E0;
     --sw-card: #FFFFFF;
     --sw-card-border: #EAEAE6;
     --sw-line: #EAEAE6;
